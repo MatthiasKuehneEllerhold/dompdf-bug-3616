ecdbfe52c2a6db316e25b26f3721f4222c94f2eb
Matthias Kühne <matthias.kuehne@ellerhold.de>
2025-05-14 14:40:28 +0200

    Reproducer

---
+++ create-pdf.php
@@ -0,0 +1,18 @@
+<?php
+
+use Dompdf\Dompdf;
+use Ellerhold\Common\Filesystem\FilesystemHelper;
+
+require __DIR__ . '/vendor/autoload.php';
+
+$domPdf = new Dompdf();
+
+$domPdf->loadHtml(file_get_contents(__DIR__ . '/input.html'));
+
+// Setup the paper size and orientation
+$domPdf->setPaper('A4', 'Portrait');
+
+// Render the HTML as PDF
+$domPdf->render();
+
+file_put_contents(__DIR__ . '/output.pdf', $domPdf->output());
